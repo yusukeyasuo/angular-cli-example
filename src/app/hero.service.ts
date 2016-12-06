e76ec93c5869a0847f237a7f5ef8b4e50422bd34
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2016-12-06 15:02:38 +0900

    tutorial #5

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
 
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
 
 }
